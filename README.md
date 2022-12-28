482c0a4ee560d837bc6be593f86f75ec2140fe19
zucar0 <42826452+zucar0@users.noreply.github.com>
2022-12-28 04:59:28 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# curso-profesional-react-codigofacilito
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-i4b3bw)
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+const App = () => {
+  return <h1>hola mundo</h1>;
+};
+
+render(<App />, document.getElementById('root'));
