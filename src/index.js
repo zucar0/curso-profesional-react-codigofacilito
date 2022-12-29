4c3ab8b1b04f0f6e5e7c6d6907694951a3abed8c
zucar0 <42826452+zucar0@users.noreply.github.com>
2022-12-29 06:06:32 -0600

    1 file modified

--- src/index.js
+++ src/index.js
@@ -2,23 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
-const Button = () => {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log('Me ejecuté');
-    return () => {
-      console.log('Adiós');
-    };
-  }, []);
-  return <button onClick={() => setCount(count + 1)}>Click {count}</button>;
+const Saludo = () => {
+  const [name, setName] = useState('');
+  return (
+    <div>
+      <input type="text" onChange={(ev) => setName(ev.target.value)} />
+      <p>Hola {name}</p>
+    </div>
+  );
 };
 
 const App = () => {
-  const [showButton, setShowButton] = useState(true);
   return (
     <div>
-      <button onClick={() => setShowButton(false)}>Eliminar botón</button>
-      <div>{showButton && <Button />}</div>
+      {' '}
+      <Saludo />{' '}
     </div>
   );
 };
